8da86b076435b1d5c63b33f69ba77714a2948742
nketia <alfred.nketia@gmail.com>
2019-06-26 17:06:44 -0300

    Mortgage record creation in database

--- config/routes.js
+++ config/routes.js
@@ -20,6 +20,7 @@ module.exports.routes = {
   ***************************************************************************/
 
   '/': { view: 'pages/homepage' },
+  '/BrokerMortgageController/createLoan_805' : 'BrokerMortgageController.createLoan_805'
 
 
   /***************************************************************************

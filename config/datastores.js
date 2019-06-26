5e969d78a89324980a2a5a68ad51adadf92ff7b4
Manpreet Singh <manpreet.singh@dal.ca>
2019-06-26 15:47:41 -0300

    Db Updated

--- config/datastores.js
+++ config/datastores.js
@@ -2,6 +2,6 @@ module.exports.datastores = {
 
   default: {
     adapter: 'sails-mysql',
-    url: 'mysql://singh2@assignment998:Mani12345@assignment998.mysql.database.azure.com:3306/assignment5_998',
+    url: 'mysql://singh2@assignment998:Mani12345@assignment998.mysql.database.azure.com:3306/assignment5_grp10',
  },
 };
